4135c61ba7f9132a73f4b154905e71fae7680689
xu.gx <xu.gx@asiainfo-sec.com>
2023-07-12 16:47:35 +0800

    删除多疑依赖饮用，代码优化调整

--- spring-boot-starter-demo-starter/src/main/java/com/xgx/springboot/config/DemoConfig.java
+++ spring-boot-starter-demo-starter/src/main/java/com/xgx/springboot/config/DemoConfig.java
@@ -2,7 +2,6 @@ package com.xgx.springboot.config;
 
 import com.xgx.springboot.entity.DemoProperties;
 import com.xgx.springboot.service.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +21,6 @@ import org.springframework.context.annotation.Configuration;
 //如下：设置demo.on=true时starter才生效。matchIfMissing = true表示全词匹配不成功则通过解析识别配置
 @ConditionalOnProperty(prefix = "demo", name = "on", havingValue = "true",matchIfMissing = true)
 public class DemoConfig {
-
-//    @Autowired
-//    private DemoProperties demoProperties;
 
     @Bean(name = "demoService")
     public DemoService demoService(DemoProperties demoProperties){
