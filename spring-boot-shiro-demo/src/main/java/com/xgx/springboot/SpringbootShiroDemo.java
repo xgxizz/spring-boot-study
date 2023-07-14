f7ca105158c4c0e595e48c1433aab3795c39e848
xu.gx <xu.gx@asiainfo-sec.com>
2023-07-14 14:18:36 +0800

    增加spring-boot-shiro模块

---
+++ spring-boot-shiro-demo/src/main/java/com/xgx/springboot/SpringbootShiroDemo.java
@@ -0,0 +1,16 @@
+package com.xgx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 服务启动类
+ * @Author xu.gx
+ * @Date 2023/7/7 17:03
+ **/
+@SpringBootApplication
+public class SpringbootShiroDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroDemo.class, args);
+    }
+}
