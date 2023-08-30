235c1d1ff030db0cc07d9327fd8ac5a4c3f30cc8
xu.gx <xu.gx@asiainfo-sec.com>
2023-08-30 15:38:57 +0800

    新增spring-boot-web-demo

--- spring-boot-web-demo/src/main/java/com/xgx/springboot/SpringbootWebDemo.java
+++ spring-boot-web-demo/src/main/java/com/xgx/springboot/SpringbootWebDemo.java
@@ -1,16 +1,19 @@
 package com.xgx.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Description: <br/>
- * @author: xgx <br/>
- * date: 2021/1/15 18:33 <br/>
- */
+ * @Description 服务启动类
+ * @Author xgx
+ * @Date 2023/8/30 10:03
+ **/
+@Slf4j
 @SpringBootApplication
-public class DemoWebApplication {
+public class SpringbootWebDemo {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoWebApplication.class, args);
+        SpringApplication.run(SpringbootWebDemo.class, args);
     }
 }
